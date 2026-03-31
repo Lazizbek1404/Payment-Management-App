3a45e2c053e408a9e65c66ff7d1ea4fa8a45fbb3
Lazizbek <sonador22@telegram>
2026-03-31 21:00:07 +0500

    Add DB connection debug logging

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,6 +2,13 @@ const { Pool } = require('pg');
 const fs = require('fs');
 const path = require('path');
 
+if (!process.env.DATABASE_URL) {
+  console.error('ERROR: DATABASE_URL environment variable is not set!');
+  process.exit(1);
+}
+
+console.log('Connecting to DB host:', new URL(process.env.DATABASE_URL).hostname);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
